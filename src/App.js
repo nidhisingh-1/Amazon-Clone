23a502ff77a0509de3323120024ad133c2796eee
Nidhi Singh <nidhi232singh@gmail.com>
2022-01-15 05:25:03 +0530

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './Header';
-import './App.css';
-import Header from './Header';
-import Home from './Home'
+import "./Header";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Home></Home>
+      <Router>
+        <Switch>
+        <Route path="/checkout">
+            <Header/>
+          <h1>Hi I am checkout</h1>
+        </Route>
+          <Route path="/">
+            <Header/>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
