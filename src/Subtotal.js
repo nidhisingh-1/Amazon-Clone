330d0625b65eb25f6c8a943b65813ff9bdb62af7
Nidhi Singh <nidhi232singh@gmail.com>
2022-01-15 06:34:59 +0530

    create subtotal section

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import './Subtotal.css'
+import CurrencyFormat from "react-currency-format";
 
 function Subtotal() {
     return (
-        <div>
-            
+        <div className='subtotal'>
+             <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              {/* Part of the homework */}
+              Subtotal (0 items): <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0} 
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"₹"}
+      />
+             <button>Proceed to Checkout</button>
+    
         </div>
     )
 }
