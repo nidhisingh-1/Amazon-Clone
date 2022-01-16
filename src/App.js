3ad85cea9135b1fc46fe691818120f230f26040b
Nidhi Singh <nidhi232singh@gmail.com>
2022-01-16 20:43:44 +0530

    login page frontend

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Header/>
+     
         <Switch>
         <Route path="/login">
-            <Checkout/>
+            <Login/>
         </Route>
-        <Route path="/checkout">
+          <Route path="/checkout">
+          <Header/>
             <Checkout/>
         </Route>
           <Route path="/">
-           
+          <Header/>
           <Home />
         </Route>
       </Switch>
